af9838f9d849a2757388148126bee589ff9fc70e
KrzysztofSwierkowski <Swierkowski.K@gmail.com>
2022-11-16 22:20:37 +0100

    -auth by firebase

---
+++ lib/auth/auth.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../auth/firebaseuser.dart';
+import '../auth/loginuser.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future signInAnonymous() async {
+    try {
+      UserCredential userCredential = await _auth.signInAnonymously();
+      User? user = await userCredential.user;
+      return _firebaseUser(user);
+    } catch (e) {
+      return FirebaseUser(code: e.toString(), uid: null);
+    }
+  }
+
+  Future signInEmailPassword(LoginUser _login) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: _login.email.toString(),
+              password: _login.password.toString());
+      User? user = userCredential.user;
+      return _firebaseUser(user);
+    } on FirebaseAuthException catch (e) {
+      return FirebaseUser(code: e.toString(), uid: null);
+    }
+  }
+
+  Future registerEmailPassword(LoginUser _login) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: _login.email.toString(),
+              password: _login.password.toString());
+      User? user = userCredential.user;
+      return _firebaseUser(user);
+    } on FirebaseAuthException catch (e) {
+      return FirebaseUser(code: e.toString(), uid: null);
+    } catch (e) {
+      return FirebaseUser(code: e.toString(), uid: null);
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  FirebaseUser? _firebaseUser(User? user){
+    return user != null ? FirebaseUser(uid: user.uid) : null;
+  }
+
+  Stream<FirebaseUser?> get user {
+    return _auth.authStateChanges().map(_firebaseUser);
+  }
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:phone_mqtt/menu.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    const MyApp(),
-  );
+import 'auth/auth.dart';
+import 'auth/firebaseuser.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp(),);
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +17,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(),
+
+    return StreamProvider<FirebaseUser?>.value(
+        value: AuthService().user,
+    initialData: null,
+    child: MaterialApp(
+    theme: ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.black,
+    buttonTheme: ButtonThemeData(
+    buttonColor: Colors.black,
+    textTheme: ButtonTextTheme.primary,
+    colorScheme:
+    Theme.of(context).colorScheme.copyWith(secondary: Colors.white),
+    ),
+    fontFamily: 'Georgia',
+    textTheme: const TextTheme(
+    headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+    headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+    bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+    ),
+    // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.cyan[600]),
+    ),
       home: const Menu(),
-    );
+    ),);
   }
 }
