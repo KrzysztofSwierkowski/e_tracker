571b18ee580732410ffbe87bf8a5795f82179aa5
Swierkowski.K@gmail.com <Krzysiu3285>
2022-12-07 23:41:32 +0100

    -add new icon -add button adding new device to track

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,6 +1,6 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 
-String topic = FirebaseAuth.instance.currentUser!.uid;
+String topic = "test";//FirebaseAuth.instance.currentUser!.uid;
 String ipBroker = "178.43.231.218"; //broker ip address
 List<String> deviceIDList = [];
