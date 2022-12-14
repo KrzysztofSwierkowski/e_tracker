b19a9145ee89dc5a27a6614040c5fd7dbeef16a5
Swierkowski.K@gmail.com <Krzysiu3285>
2022-12-14 15:27:08 +0100

    -add new icon -add button adding new device to track

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,6 +1,12 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 
-String topic = "test";//FirebaseAuth.instance.currentUser!.uid;
+String topic = FirebaseAuth.instance.currentUser!.uid;
+String topicSpeed = "gpsDevice/GPS001/speed";
+String topicAltitude = "gpsDevice/GPS001/altitude";
+String topicLongLat = "gpsDevice/GPS001/longLat";
+String topicInit = "gpsDevice/GPS001/state";
+String topicIP = "gpsDevice/GPS001/ip";
+
 String ipBroker = "178.43.231.218"; //broker ip address
 List<String> deviceIDList = [];
