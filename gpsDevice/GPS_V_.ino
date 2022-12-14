9aa9adc23e6b90257b19df7bb3c2ab7444486422
Krzysztof <34037676+KrzysztofSwierkowski@users.noreply.github.com>
2022-12-14 09:00:01 +0100

    Add files via upload

---
+++ gpsDevice/GPS_V_.ino
@@ -0,0 +1,356 @@
+/**************************************************************
+
+   For this example, you need to install PubSubClient library:
+     https://github.com/knolleary/pubsubclient
+     or from http://librarymanager/all#PubSubClient
+
+   TinyGSM Getting Started guide:
+     https://tiny.cc/tinygsm-readme
+
+   For more MQTT examples, see PubSubClient library
+
+ **************************************************************
+   Use Mosquitto client tools to work with MQTT
+     Ubuntu/Linux: sudo apt-get install mosquitto-clients
+     Windows:      https://mosquitto.org/download/
+
+   Subscribe for messages:
+     mosquitto_sub -h test.mosquitto.org -t GsmClientTest/init -t GsmClientTest/ledStatus -q 1
+   Toggle led:
+     mosquitto_pub -h test.mosquitto.org -t GsmClientTest/led -q 1 -m "toggle"
+
+   You can use Node-RED for wiring together MQTT-enabled devices
+     https://nodered.org/
+   Also, take a look at these additional Node-RED modules:
+     node-red-contrib-blynk-ws
+     node-red-dashboard
+
+ **************************************************************/
+
+// Please select the corresponding model
+
+#define SIM800L_IP5306_VERSION_20190610
+// #define SIM800L_AXP192_VERSION_20200327
+// #define SIM800C_AXP192_VERSION_20200609
+// #define SIM800L_IP5306_VERSION_20200811
+
+#include "utilities.h"
+
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800
+#include <TinyGPS++.h>
+
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Set serial for AT commands (to the module)
+// Use Hardware Serial on Mega, Leonardo, Micro
+#define Serialatitude Serial1
+
+// See all AT commands, if wanted
+//#define DUMP_AT_COMMANDS
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+
+//------------------------------------------------------------------------------------
+// RX TX GPS
+//------------------------------------------------------------------------------------
+
+#define RXD2 32
+#define TXD2 33
+
+//------------------------------------------------------------------------------------
+// TINY GPS PLUS
+//------------------------------------------------------------------------------------
+
+#define GPS Serial2
+TinyGPSPlus gps;
+
+// GPS VARIABLES
+//------------------------------------------------------------------------------------
+
+double latitude = 0.00;
+double longitude = 0.00;
+double speed = 0.00;
+const char idDevice[] = "GPS001";
+bool locationIsValid = true;
+
+// Add a reception delay - may be needed for a fast processor at a slow baud rate
+// #define TINY_GSM_YIELD() { delay(2); }
+
+// Define how you're planning to connect to the internet
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
+
+// set GSM PIN, if any
+#define GSM_PIN ""
+
+// Your GPRS credentials, if any
+const char apn[] = "internet";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+// MQTT details
+const char *broker = "178.43.231.218";
+
+const char *topicSpeed = "gpsDevice/GPS001/speed";
+const char *topicAltitude = "gpsDevice/GPS001/altitude";
+const char *topicLongLat = "gpsDevice/GPS001/longLat";
+const char *topicInit = "gpsDevice/GPS001/state";
+
+
+//const char *topicGps = "test";
+
+#include <TinyGsmClient.h>
+#include <PubSubClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(Serialatitude, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(Serialatitude);
+#endif
+TinyGsmClient client(modem);
+PubSubClient mqtt(client);
+
+int ledStatus = LOW;
+
+uint32_t lastReconnectAttempt = 0;
+
+
+
+//void mqttCallback(char *topic, byte *payload, unsigned int len)
+
+void mqttCallback(char *topic, byte *payload, unsigned int len)
+{
+  SerialMon.print("Message arrived [");
+  SerialMon.print(topic);
+  SerialMon.print("]: ");
+  SerialMon.write(payload, len);
+  SerialMon.println();
+
+  // Only proceed if incoming message's topic matches
+  if (mqtt.connected()) {
+    ledStatus = !ledStatus;
+    digitalWrite(LED_GPIO, ledStatus);
+    //mqtt.publish(topicLongLat, ledStatus ? "1" : "0");
+
+    if (gps.location.isValid()) {
+      double latitude = (gps.location.lat());
+      double longitude = (gps.location.lng());
+
+      Serial.println("********** Publish MQTT data to TENTEGO");
+      char mqtt_payload[50] = "";
+      // {\"latitude\":${currentLocation!.latitude.toString()},\"longitude\":${currentLocation!.longitude.toString()}}
+      snprintf (mqtt_payload, 50, "{\"latitude\":%lf,\"longitude\":%lf}", latitude, longitude);
+      Serial.print("Publish message: ");
+      Serial.println(mqtt_payload);
+      mqtt.publish(topicLongLat, mqtt_payload);
+      Serial.println("> MQTT data published");
+      Serial.println("********** End ");
+      Serial.println("*****************************************************");
+
+      delay(1000);// delay
+    } else {
+      Serial.println(F("INVALID"));
+    }
+  }
+}
+
+boolean mqttConnect()
+{
+  SerialMon.print("Connecting to ");
+  SerialMon.print(broker);
+
+  // Connect to MQTT Broker
+  //boolean status = mqtt.connect("GsmClientTest");
+
+  // Or, if you want to authenticate MQTT:
+  boolean status = mqtt.connect("test", "rstt", "3333");
+
+  if (status == false) {
+    SerialMon.println(" fail");
+    return false;
+  }
+  SerialMon.println(" success");
+  mqtt.publish(topicInit, "GsmClientTest started");
+  //mqtt.subscribe(topicLed);
+  return mqtt.connected();
+}
+
+
+void setup()
+{
+  // Set console baud rate
+  SerialMon.begin(115200);
+
+  delay(10);
+
+  setupModem();
+
+  SerialMon.println("Wait...");
+
+  // Set GSM module baud rate and UART pins
+  Serialatitude.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+
+  delay(6000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // modem.init();
+
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print("Modem Info: ");
+  SerialMon.println(modemInfo);
+
+#if TINY_GSM_USE_GPRS
+  // Unlock your SIM card with a PIN if needed
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+  }
+#endif
+
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isNetworkConnected()) {
+    SerialMon.println("Network connected");
+  }
+
+  // GPRS connection parameters are usually set after network registration
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isGprsConnected()) {
+    SerialMon.println("GPRS connected");
+  }
+
+  // MQTT Broker setup
+  mqtt.setServer(broker, 1883);
+  mqtt.setCallback(mqttCallback);
+
+
+  GPS.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("[GPS]: Serial initialize");
+
+
+}
+
+
+static void searchGPS(unsigned long ms)
+{
+
+  unsigned long start = millis();
+
+  do
+  {
+
+    while (GPS.available() > 0)
+    {
+      gps.encode(GPS.read());
+
+      if (gps.location.isValid())
+      {
+
+        locationIsValid = true;
+
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+        speed = gps.speed.kmph();
+      }
+      else
+      {
+        speed = 0.00;
+        locationIsValid = false;
+      }
+
+    }
+
+  } while (millis() - start < ms);
+}
+
+
+void loop()
+{
+
+  if (!mqtt.connected()) {
+    SerialMon.println("=== MQTT NOT CONNECTED ===");
+    // Reconnect every 10 seconds
+    uint32_t t = millis();
+    if (t - lastReconnectAttempt > 10000L) {
+      lastReconnectAttempt = t;
+      if (mqttConnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+    delay(100);
+    return;
+  }
+  searchGPS(1000);
+  SerialMon.println("loop");
+  checkGps();
+  sendLocation();
+  mqtt.loop();
+  //  SerialMon.print(topic);
+}
+
+
+void sendLocation()
+{
+  if (mqtt.connected()) {
+    ledStatus = !ledStatus;
+    digitalWrite(LED_GPIO, ledStatus);
+    //mqtt.publish(topicLongLat, ledStatus ? "1" : "0");
+
+    if (gps.location.isValid()) {
+      double latitude = (gps.location.lat());
+      double longitude = (gps.location.lng());
+
+      Serial.println("********** Publish MQTT data to TENTEGO");
+      char mqtt_payload[50] = "";
+      // {\"latitude\":${currentLocation!.latitude.toString()},\"longitude\":${currentLocation!.longitude.toString()}}
+      snprintf (mqtt_payload, 50, "{\"latitude\":%lf,\"longitude\":%lf}", latitude, longitude);
+      Serial.print("Publish message: ");
+      Serial.println(mqtt_payload);
+      mqtt.publish(topicLongLat, mqtt_payload);
+      Serial.println("> MQTT data published");
+      Serial.println("********** End ");
+      Serial.println("*****************************************************");
+
+      delay(1000);// delay
+    } else {
+      Serial.println(F("INVALID"));
+    }
+  }
+}
+
+void checkGps(){
+
+// Debug: if we haven't seen lots of data in 5 seconds, something's wrong.
+if (millis() > 5000 && gps.charsProcessed() < 10) // uh oh
+{
+  Serial.println("ERROR: not getting any GPS data!");
+  // dump the stream to Serial
+  Serial.println("GPS stream dump:");
+  while (true) // infinite loop
+    if (GPS.available() > 0) // any data coming in?
+      Serial.write(GPS.read());
+}
+
+}
